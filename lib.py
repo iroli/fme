1a46821f518d0b9cc04c4a99ee7fd91223083334
ShFsn <tigr.tigrov.1977@gmail.com>
2024-01-27 15:44:37 +0300

    Relations search: word endings supression, non-strict sequencing, bruteforce mode, multiprocessing

---
+++ lib.py
@@ -0,0 +1,366 @@
+# 0. Общий код
+
+# noinspection PyUnresolvedReferences
+from os import walk
+# noinspection PyUnresolvedReferences
+from xml.etree import ElementTree
+# noinspection PyUnresolvedReferences
+from xml.dom import minidom
+# noinspection PyUnresolvedReferences
+import re
+# noinspection PyUnresolvedReferences
+import codecs
+# noinspection PyUnresolvedReferences
+from transliterate import translit, get_available_language_codes
+# noinspection PyUnresolvedReferences
+from random import randint
+# noinspection PyUnresolvedReferences
+import enchant
+# noinspection PyUnresolvedReferences
+from enchant.checker import SpellChecker
+# noinspection PyUnresolvedReferences
+from enchant.tokenize import EmailFilter, URLFilter
+# noinspection PyUnresolvedReferences
+import difflib
+# noinspection PyUnresolvedReferences
+import itertools
+# noinspection PyUnresolvedReferences
+from tqdm.auto import tqdm
+# noinspection PyUnresolvedReferences
+from multiprocessing import Pool
+# noinspection PyUnresolvedReferences
+import os
+
+
+# Small dictionaries merger
+def dict_merge(dict1: dict, dict2: dict) -> dict:
+    dict0 = {}
+    for key_local in dict1.keys():
+        dict0[key_local] = dict1[key_local]
+    for key_local in dict2.keys():
+        dict0[key_local] = dict2[key_local]
+    return dict0
+
+
+# -------------------------- VARS --------------------------------
+# Symbols and combinations that have to be corrected after OCR
+COMBINATIONS_CORR_ALPHABET = {
+    'A': 'А', 'a': 'а', 'B': 'В', 'b': 'Ь', 'C': 'С', 'c': 'с', 'E': 'Е', 'e': 'е', 'H': 'Н', 'K': 'К', 'M': 'М',
+    'O': 'О', 'P': 'Р', 'p': 'р', 'T': 'Т', 'X': 'Х', 'y': 'у', 'x': 'х',
+    'U': 'И',
+    'u': 'и',
+    'r': 'г',
+    'N': 'П',
+    'n': 'п',
+    'm': 'т',
+    'Y': 'У',
+    # 'S' : 'Я',		# Seems irrelevant
+}
+COMBINATIONS_CORR_UNICODE = {
+    'І': 'I',  # These two "I" are different!
+    'ก': 'п',
+    '山': 'Ц',
+    'כ': 'э',
+    'חи': 'пи',
+    'प': 'Ч',
+    'иั': 'й'
+}
+COMBINATIONS_CORR_OTHER = {
+    ' -': '-',
+    '- ': '-',
+    '0': 'О',
+    '3': 'З',
+    '6': 'б',
+}
+COMBINATIONS_CORR_GLOBAL = dict_merge(COMBINATIONS_CORR_ALPHABET,
+                                      dict_merge(COMBINATIONS_CORR_UNICODE, COMBINATIONS_CORR_OTHER))
+# Symbols excluded in xml have to be converted back
+XML_EXCLUDES = {
+    '&quot;': '"',
+    '&apos;': "'",
+    '&lt;': '<',
+    '&gt;': '>',
+    '&amp;': '&'
+}
+PERSONAL_WORD_LIST = "./matphys/PWL.txt"
+URI_PREFIX = "http://libmeta.ru/fme/"
+# ----------------------------------------------------------------
+
+
+# Get names of files in given directory
+def get_filenames(directory: str) -> list:
+    return next(walk(directory), (None, None, []))[2]  # [] if no file
+
+
+# Write xml tree to file
+def prettify(element_tree: ElementTree.Element) -> str:
+    # Pretty-printed XML string for the Element.
+    rough_string = ElementTree.tostring(element_tree, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
+
+
+def xml_write(element_tree: ElementTree.Element, file_name: str):
+    with codecs.open(file_name, 'w', 'utf-8') as f_out:
+        f_out.write(prettify(element_tree))
+
+
+# Convert xml excluded symbols
+def xml_excluded_convert(text_to_process: str) -> str:
+    for key_local in XML_EXCLUDES.keys():
+        while text_to_process.find(key_local) != -1:
+            pos_local = text_to_process.find(key_local)
+            text_to_process = text_to_process[:pos_local] + XML_EXCLUDES[key_local] + text_to_process[
+                                                                                      pos_local + len(key_local):]
+    return text_to_process
+
+
+def remove_xml_spaces(element_tree: ElementTree.Element, file_name: str) -> ElementTree.Element:
+    element_tree.tail = None
+    if element_tree.text is not None:
+        is_space = True
+        for letter in element_tree.text:
+            is_space = False if letter != ' ' else is_space
+        if is_space:
+            element_tree.text = None
+        else:
+            if element_tree.tag == 'text':
+                element_tree.text = get_texts(file_name)[0]
+            elif element_tree.tag == 'text_orig':
+                element_tree.text = get_texts(file_name)[1]
+            element_tree.text = xml_excluded_convert(element_tree.text)
+    for subelem in element_tree:
+        subelem = remove_xml_spaces(subelem, file_name)
+        subelem.tail = None  # A dummy action to get item used for pylint
+    return element_tree
+
+
+def parse_xml(file_name: str) -> ElementTree.Element:
+    # Parse existing xml (string parsing is needed to avoid extra newlines appearing)
+    exit_string = ''
+    with codecs.open(file_name, 'r', 'utf-8') as f_in:
+        for line in f_in.readlines():
+            exit_string += line[:-1]
+    et_root = ElementTree.fromstring(exit_string)
+    et_root = remove_xml_spaces(et_root, file_name)
+    return et_root
+
+
+# !!!BUG!!! for some reason newlines disappear in texts in parsed xml, so extract article texts manually and replace
+def get_texts(file_name: str) -> tuple[str, str]:
+    with codecs.open(file_name, 'r', 'utf-8') as f_in:
+        file_content = f_in.read()
+    text_local = file_content[file_content.find('<text>') + 6:file_content.find('</text>')]
+    with codecs.open(file_name, 'r', 'utf-8') as f_in:
+        file_content = f_in.read()
+    text_orig = file_content[file_content.find('<text_orig>') + 11:file_content.find('</text_orig>')]
+    return text_local, text_orig
+
+
+# Get xml tree element with certain tag name
+def get_xml_elem(element: ElementTree.Element, elem_path: str) -> ElementTree.Element:
+    tgt = elem_path.split('/')[0]
+    for subelem in element:
+        if subelem.tag == tgt:
+            if elem_path.find('/') != -1:
+                return get_xml_elem(subelem, elem_path[elem_path.find('/') + 1:])
+            else:
+                return subelem
+    return element
+
+
+# Titles handling functions
+# Correct preferred combinations and latin letters
+def title_handle_latin(new_title: str, HANDLER_COMBINATIONS_CORR: dict) -> str:
+    if new_title is None or len(new_title) == 0:
+        return ''
+    for combi in HANDLER_COMBINATIONS_CORR.keys():
+        while new_title.find(combi) != -1:
+            new_title = new_title[:new_title.find(combi)] + \
+                        HANDLER_COMBINATIONS_CORR[combi] + new_title[new_title.find(combi) + len(combi):]
+    return new_title
+
+
+# Remove bounding symbols
+def title_handle_bounding(new_title: str) -> str:
+    if new_title is None or len(new_title) == 0:
+        return ''
+    return new_title.strip("!#%&'*+-.^_`|~:;")
+
+
+# Merge single-lettered words
+def title_handle_merge(new_title: str) -> str:
+    if new_title is None or len(new_title) == 0:
+        return ''
+    new_title = ' ' + new_title + ' '
+    for p in range(len(new_title) - 4):
+        if (new_title[p] == ' ' or new_title[p] == '№') and new_title[p + 2] == ' ' and new_title[p + 4] == ' ':
+            new_title = new_title[:p + 2] + '№' + new_title[p + 3:]
+    p = 0
+    while p < len(new_title):
+        if new_title[p] == '№':
+            new_title = new_title[:p] + new_title[p + 1:]
+            p = 0
+        else:
+            p += 1
+    while new_title[0] == ' ':
+        new_title = new_title[1:]
+    while new_title[-1] == ' ':
+        new_title = new_title[:-1]
+    return new_title
+
+
+# Revert changes for aux formulas in titles
+def title_handle_formulas(new_title: str, old_title: str) -> str:
+    if new_title is None or len(new_title) == 0:
+        return ''
+    pos_old = 0
+    pos_new = 0
+    while old_title.find('$', pos_old) != -1 and new_title.find('$', pos_new) != -1:
+        pos_old = old_title.find('$', pos_old) + 1
+        pos_new = new_title.find('$', pos_new) + 1
+        pos_old_next = old_title.find('$', pos_old) if old_title.find('$', pos_old) != -1 else len(old_title)
+        pos_new_next = new_title.find('$', pos_new) if new_title.find('$', pos_new) != -1 else len(new_title)
+        new_title = new_title[:pos_new] + old_title[pos_old:pos_old_next] + new_title[pos_new_next:]
+        pos_old = pos_old_next + 1
+        pos_new = (new_title.find('$', pos_new) if new_title.find('$', pos_new) != -1 else len(new_title)) + 1
+    while new_title[-2:] == '-$' or new_title[-2:] == ',$' or new_title[-2:] == ':$':
+        new_title = new_title[:-2] + '$'
+    return new_title
+
+
+# Position checkers
+# Checks if given position is between opening and closing scopes
+def check_in_scopes(text_to_check: str, pos_to_check: int, opening_scope: str, closing_scope: str) -> bool:
+    if text_to_check is None:
+        return False
+    open_prev = text_to_check.rfind(opening_scope, 0, pos_to_check)
+    close_prev = text_to_check.rfind(closing_scope, 0, pos_to_check)
+    open_next = text_to_check.find(opening_scope, pos_to_check)
+    close_next = text_to_check.find(closing_scope, pos_to_check)
+    after_open = True if (
+            (open_prev != -1 and close_prev == -1) or (open_prev > close_prev != -1 and open_prev != -1)) else False
+    before_close = True if ((open_next == -1 and close_next != -1) or (
+            open_next > close_next != -1 and open_next != -1)) else False
+    return after_open and before_close
+
+
+def check_in_uri(text_to_check: str, pos_to_check: int) -> bool:
+    return check_in_scopes(text_to_check, pos_to_check, 'URI[[', ']]/URI')
+
+
+def check_in_link(text_to_check: str, pos_to_check: int) -> bool:
+    return check_in_scopes(text_to_check, pos_to_check, '![](', ')')
+
+
+def check_in_formula(text_to_check: str, pos_to_check: int) -> bool:
+    # Main formulas
+    in_main = check_in_scopes(text_to_check, pos_to_check, '\\[', '\\]')
+    # Aux formulas
+    if text_to_check is None:
+        return False
+    p_find = 0
+    cntr = 0
+    found_before = 0
+    # Count dollar symbols and find target position
+    while text_to_check.find('$', p_find) != -1:
+        p_find = text_to_check.find('$', p_find)
+        cntr += 1
+        if not found_before and pos_to_check <= p_find:
+            found_before = cntr
+        p_find += 1
+    # If cnt is not even assume that first one is garbage from title
+    in_aux = not ((found_before + cntr) % 2) and found_before > 0
+    return in_main or in_aux
+
+
+# Prepare spellcheckers
+ru_dict = enchant.DictWithPWL("ru_RU", PERSONAL_WORD_LIST)
+ru_checker = SpellChecker(ru_dict, filters=[EmailFilter, URLFilter])
+en_dict = enchant.DictWithPWL("en_US", PERSONAL_WORD_LIST)
+en_checker = SpellChecker(en_dict, filters=[EmailFilter, URLFilter])
+
+
+def spellcheck_dict_update():
+    global ru_dict
+    global ru_checker
+    global en_dict
+    global en_checker
+    ru_dict = enchant.DictWithPWL("ru_RU", PERSONAL_WORD_LIST)
+    ru_checker = SpellChecker(ru_dict, filters=[EmailFilter, URLFilter])
+    en_dict = enchant.DictWithPWL("en_US", PERSONAL_WORD_LIST)
+    en_checker = SpellChecker(en_dict, filters=[EmailFilter, URLFilter])
+
+
+# Use PyEnchant spellchecker
+def do_spellcheck(text_to_check: str) -> dict:
+    global ru_dict
+    global ru_checker
+    global en_dict
+    global en_checker
+    dictionaries = [ru_dict, en_dict]
+    checkers = [ru_checker, en_checker]
+    text_suggestions_local = dict()
+
+    # Spellcheck
+    for p in range(len(checkers)):
+        checker = checkers[p]
+        dictionary = dictionaries[p]
+
+        checker.set_text(text_to_check)
+        for woi in checker:
+            # Exclude some wois to reduce computation time and output
+            if check_in_uri(text_to_check, woi.wordpos) or check_in_link(text_to_check, woi.wordpos) or \
+                    check_in_formula(text_to_check, woi.wordpos) or len(woi.word) < 4 or \
+                    woi.wordpos in text_suggestions_local.keys() or \
+                    text_to_check[min(woi.wordpos + len(woi.word), len(text_to_check) - 1)] in ['.']:
+                continue
+            # Ignore "...x- x..." cases
+            if woi.wordpos >= 3 and text_to_check[woi.wordpos - 1] == ' ' and \
+                    text_to_check[woi.wordpos - 2] == '-' and text_to_check[woi.wordpos - 3] != ' ':
+                continue
+            if woi.wordpos + len(woi.word) + 2 < len(text_to_check) and \
+                    text_to_check[woi.wordpos + len(woi.word)] == '-' and \
+                    text_to_check[woi.wordpos + len(woi.word) + 1] == ' ' and \
+                    text_to_check[woi.wordpos + len(woi.word) + 2] != ' ':
+                continue
+            # Check if word is correct in some other language
+            word_is_correct_in_other_dict = False
+            for _dictionary in dictionaries:
+                word_is_correct_in_other_dict = True if _dictionary.check(woi.word) else word_is_correct_in_other_dict
+            if word_is_correct_in_other_dict:
+                continue
+            # Generate a suggestion
+            sim = dict()
+            word_suggestions = set(dictionary.suggest(woi.word))
+            for word_local in word_suggestions:
+                measure = difflib.SequenceMatcher(None, woi.word, word_local).ratio()
+                if len(woi.word) == len(word_local):
+                    measure += 2.0  # Give priority to length-constant suggestions (i.e. with no extra whitespaces)
+                    cnt_err = 0
+                    cnt_err_sel = 0
+                    for letter in range(len(word_local)):
+                        if woi.word[letter] != word_local[letter]:
+                            cnt_err += 1
+                            if (woi.word[letter] == 'ш' or woi.word[letter] == 'щ') and (
+                                    word_local[letter] == 'ш' or word_local[letter] == 'щ'):
+                                cnt_err_sel += 1
+                    measure += 2.0 if cnt_err == cnt_err_sel else 0.0  # Give even more priority in "ш-щ" case
+                # Write measure
+                sim[measure] = word_local
+            suggest = sim[max(sim.keys())] if len(sim.keys()) else None
+            # Exclude some wois to reduce computation time and output
+            if suggest is None or suggest == woi.word:
+                continue
+            else:
+                text_suggestions_local[woi.wordpos] = (woi.word, suggest)
+
+    return text_suggestions_local
+
+
+def add_to_pwl(word_to_add: str):
+    with codecs.open(PERSONAL_WORD_LIST, 'r', 'utf-8') as f_in:
+        pwl = f_in.read()
+    if pwl.find(f"\n{word_to_add}\n") == -1:
+        with codecs.open(PERSONAL_WORD_LIST, 'a', 'utf-8') as f_out:
+            f_out.write(f"{word_to_add.strip()}\n")
--- matphys/FMEformulas_check.md
+++ matphys/FMEformulas_check.md
@@ -1,40 +1,40 @@
-1. Статья: http://libmeta.ru/fme/article/2882_SPEKTRAL'NAJa, Начало на стр. 560, формула 1:
-$$E_{\lambda}=E((-\infty, \lambda)), \lambda \in \mathbb{R} \text {. }$$
-2. Статья: http://libmeta.ru/fme/article/1503_LAGRANZhA, Начало на стр. 310, формула 3:
-$$\begin{gathered}N_{v x}=\sum_{i=1}^{k} \lambda_{i} \frac{\partial f_{i}}{\partial x_{v}}, \quad N_{v y}=\sum_{i=1}^{k} \lambda_{i} \frac{\partial f_{i}}{\partial y_{v}}, \\N_{v z}=\sum_{i=1}^{k} \lambda_{i} \frac{\partial f_{i}}{\partial z_{v}} .\end{gathered}$$
-3. Статья: http://libmeta.ru/fme/article/1524_LAKSA, Начало на стр. 316, формула 3:
-$$R(E, w)=\operatorname{det}(L(t, E)-w \cdot 1)=0 .$$
-4. Статья: http://libmeta.ru/fme/article/1617_LIPPMANA-ShVINGERA, Начало на стр. 328, формула 2:
-$$R(z)=R_{0}(z)-R_{0}(z) V R(z),$$
-5. Статья: http://libmeta.ru/fme/article/2135_OSTsILLJaTsIJ, Начало на стр. 419, формула 1:
-$$\begin{gathered}h^{2} u_{t t}+L\left(x,-i h \partial / \partial_{x}\right) u=0, \\L(x, p)=4 c^{2}(x) \sin ^{2} p / 2, x \in \mathbb{R}^{1},-\pi \leqslant p \leqslant \pi,\end{gathered}$$
-6. Статья: http://libmeta.ru/fme/article/2445_$1_H$-PSEVDODIFFERENTsIAL'NYJ, Начало на стр. 470, формула 2:
-$$\left|D_{x}^{\alpha} D_{p}^{\beta} H(x, p, h)\right| \leqslant C_{\alpha \beta}(1+|x|+|p|)^{m}$$
-7. Статья: http://libmeta.ru/fme/article/972_IZOBRAZhENIJ, Начало на стр. 208, формула 3:
-$$\Gamma=\{(x, y): y=0,-\infty<x<+\infty\}$$
-8. Статья: http://libmeta.ru/fme/article/445_VOLNOVAJa, Начало на стр. 97, формула 6:
-$$\begin{aligned}& \int d \rho_{A}\left(a_{1}, \ldots, a_{N}\right) \psi_{1}^{*}\left(a_{1}, \ldots, a_{N}\right) \psi_{2}\left(a_{1}, \ldots, a_{N}\right)= \\& =\int d \rho_{B}\left(b_{1}, \ldots, b_{M}\right) c_{1}^{*}\left(b_{1}, \ldots, b_{M}\right) c_{2}\left(b_{1}, \ldots, b_{M}\right) .\end{aligned}$$
-9. Статья: http://libmeta.ru/fme/article/447_VOLNOVOE, Начало на стр. 99, формула 11:
-$$S_{t}^{ \pm}+H^{ \pm}\left(x, \nabla S^{ \pm}\right)=0,\left.S^{ \pm}\right|_{t=0}=\omega x,$$
-10. Статья: http://libmeta.ru/fme/article/2702_SVJaZANNOE, Начало на стр. 523, формула 1:
-$$\varepsilon_{\mathrm{CB}}=\Delta m c^{2}$$
-11. Статья: http://libmeta.ru/fme/article/1539_LANDAU-HOPFA, Начало на стр. 319, формула 1:
+1. Статья: http://libmeta.ru/fme/article/2325_POMERANChUKA, Начало на стр. 455, формула 2:
+$$\Delta \sigma=\sigma_{\text {полн }}^{\bar{a} b}(E)-\sigma_{\text {полн }}^{a b}(E)$$
+2. Статья: http://libmeta.ru/fme/article/3441_ShVARTsA, Начало на стр. 667, формула 1:
+$$|(x, y)|^{2} \leqslant(x, x)(y, y) .$$
+3. Статья: http://libmeta.ru/fme/article/823_DINAMIChESKAJa, Начало на стр. 173, формула 4:
+$$\frac{d}{d t} \Phi_{t}(X)=\mathscr{P}\left(\Phi_{t}(X)\right)$$
+4. Статья: http://libmeta.ru/fme/article/1315_KOMMUTATsII, Начало на стр. 281, формула 1:
+$$L(\varphi(x) \exp \{i \lambda S(x)\})=\exp \{i \lambda S(x)\} \sum_{j=0}^{m}(i \lambda)^{-j}\left(R_{j} \varphi\right)(x),$$
+5. Статья: http://libmeta.ru/fme/article/1444_KRAEVAJa, Начало на стр. 302, формула 9:
+$$B u(y) \equiv \sum_{i=1}^{n} a_{i}(y) \frac{\partial u}{\partial y_{i}}-f(y),$$
+6. Статья: http://libmeta.ru/fme/article/3365_HOPFA, Начало на стр. 657, формула 3:
+$$(u, v)=\int_{\Omega} u^{j}(x) v^{j}(x) d x$$
+7. Статья: http://libmeta.ru/fme/article/1220_KERRA-N'JuMENA, Начало на стр. 265, формула 2:
+$$\Delta=r^{2}-2 m r+a^{2}+Q^{2}, \Sigma=r^{2}+a^{2} \cos ^{2} \theta .$$
+8. Статья: http://libmeta.ru/fme/article/1197_KVANTOVYE, Начало на стр. 260, формула 1:
+$$A=A^{(0)}+\hbar A^{(1)}+\hbar A^{(2)}+\ldots$$
+9. Статья: http://libmeta.ru/fme/article/571_GEL'FANDA-LERE, Начало на стр. 125, формула 1:
+$$\omega=d f \wedge \omega_{1} \text {. }$$
+10. Статья: http://libmeta.ru/fme/article/1539_LANDAU-HOPFA, Начало на стр. 319, формула 1:
 $$u\left[x, \varphi_{1}(t), \ldots, \varphi_{N}(t)\right]$$
-12. Статья: http://libmeta.ru/fme/article/959_ZVUKOVYE, Начало на стр. 204, формула 7:
-$$\frac{\partial^{2}}{\partial t^{2}} \delta p-c^{2} \frac{\partial^{2}}{\partial x^{2}} \delta p=0$$
-13. Статья: http://libmeta.ru/fme/article/860_DISSIPATIVNAJa, Начало на стр. 187, формула 5:
-$$\Phi=\mu \sum_{i=1}^{3} \sum_{k=1}^{3} \varepsilon_{i k}^{2}+\frac{1}{2}\left(\lambda-\frac{2}{3} \mu\right) \theta^{2},$$
-14. Статья: http://libmeta.ru/fme/article/2588_REZOL'VENTA, Начало на стр. 500, формула 2:
-$$\hat{U}(t)=(2 \pi)^{-1} \int_{C} d z e^{-i z t} \hat{\Re}(z) .$$
-15. Статья: http://libmeta.ru/fme/article/1022_INSTANTO, Начало на стр. 215, формула 11:
-$$A_{\mu}(x)=V_{i} \lambda_{i}^{2}\left(\left(x-a_{i}\right)^{2}+\lambda_{i}^{2}\right)^{-1} u^{-1}\left(\frac{x-a_{i}}{\left|x-a_{i}\right|}\right) d_{\mu} u\left(\frac{x-a_{i}}{\left|x-a_{i}\right|}\right),$$
-16. Статья: http://libmeta.ru/fme/article/2833_SMOLUHOVSKOGO, Начало на стр. 549, формула 2:
-$$\begin{gathered}\frac{\partial f}{\partial t}+v \frac{\partial f}{\partial x}=\frac{1}{2} \int_{0}^{p} \Phi(p-y, y) f(p-y) f(y) d y- \\\ddots-f(x, p, t) \int_{0}^{\infty} \Phi(p, y) f(p) d p .\end{gathered}$$
-17. Статья: http://libmeta.ru/fme/article/984_IZOTOPIChESKAJa, Начало на стр. 211, формула 1:
-$$\begin{aligned}u^{\prime} & =a_{11} u+a_{12} d, \\d^{\prime} & =a_{21} u+a_{22} d,\end{aligned}$$
-18. Статья: http://libmeta.ru/fme/article/3227_FADDEEVA, Начало на стр. 633, формула 13:
-$$T_{\alpha}(z) R_{0}(z) T_{\beta}(z) R_{0}(z)$$
-19. Статья: http://libmeta.ru/fme/article/2849_SOVMESTIMYE, Начало на стр. 552, формула 4:
-$$\mu_{\rho}(B)=\operatorname{Tr} \rho E(B) ; B \in \mathscr{B}\left(\mathbb{R}^{n}\right) .$$
-20. Статья: http://libmeta.ru/fme/article/3317_FUNDAMENTAL'NOE, Начало на стр. 646, формула 1:
-$$P(D) E=\delta,$$
+11. Статья: http://libmeta.ru/fme/article/1709_MANDELJa, Начало на стр. 344, формула 3:
+$$P(n)=\frac{\varepsilon^{n}}{n !} e^{-\varepsilon} .$$
+12. Статья: http://libmeta.ru/fme/article/3252_FAZOVYH, Начало на стр. 637, формула 3:
+$$\delta_{l}(k)=\lim _{R \rightarrow \infty} \delta_{l}(k, R)$$
+13. Статья: http://libmeta.ru/fme/article/1462_KRIVIZNY, Начало на стр. 307, формула 1:
+$$d \Omega=d \theta+\frac{1}{2}[\theta, \theta] .$$
+14. Статья: http://libmeta.ru/fme/article/1563_LEZhANDRA, Начало на стр. 322, формула 1:
+$$p=\partial s / \partial q, u=\langle p, q\rangle-S(q), p \in V^{*}, q \in V, u \in \mathbb{R} .$$
+15. Статья: http://libmeta.ru/fme/article/618_GIDROMAGNITNAJa, Начало на стр. 136, формула 1:
+$$\int\left(\frac{\rho v^{2}}{2}+\frac{H^{2}}{8 \pi}\right) d x d y, \quad \int A^{2} d x d y, \quad \int \boldsymbol{v} \boldsymbol{H} d x d y$$
+16. Статья: http://libmeta.ru/fme/article/442_VOZMUSchENIJ, Начало на стр. 94, формула 9:
+$$\ddot{q}_{i}^{(k)}+\sum_{j=1}^{3} p_{i j} q_{j}^{(k)}=P_{i}^{(k)}$$
+17. Статья: http://libmeta.ru/fme/article/843_DIRAKA, Начало на стр. 180, формула 1:
+$$\psi(x)=\left(\begin{array}{l}\psi_{1}(x) \\\psi_{2}(x) \\\psi_{3}(x) \\\psi_{4}(x)\end{array}\right)$$
+18. Статья: http://libmeta.ru/fme/article/1409_KORRELJaTsIONNAJa, Начало на стр. 295, формула 2:
+$$\Xi(\Lambda, z, \beta) \sum_{n=0}^{\infty} \frac{z^{n}}{n !} \int d x_{1} \ldots d x_{n} e^{-\beta U(x)_{n}} .$$
+19. Статья: http://libmeta.ru/fme/article/2955_STEPENI, Начало на стр. 580, формула 1:
+$$f=k-\varphi+2 \text {, }$$
+20. Статья: http://libmeta.ru/fme/article/2924_STATISTIChESKAJa, Начало на стр. 569, формула 25:
+$$\partial \chi(t, w) / \partial t=\int \exp \{i(u, w)\}(b(t, u), w) \mu(t, d u)$$
